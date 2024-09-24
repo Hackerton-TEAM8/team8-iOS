7c1bdbcfbaac0317cdbb26e66e1bb1e127a58669
realhsb <realhsb@gmail.com>
2024-09-25 00:07:39 +0900

    🎉: 프로젝트 파일 생성

---
+++ project/project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by Subeen on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
