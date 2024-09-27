21729aaef33c71625715b32d938fca97761e3dcc
CJiu01 <jiwoo020801@gmail.com>
2024-09-27 17:33:58 +0900

    feat: SearchView 추가

--- project/project/Source/View/Search/SearchView.swift
+++ project/project/Source/View/Search/SearchView.swift
@@ -5,4 +5,14 @@
 //  Created by 최지우 on 9/27/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("SearchView")
+    }
+}
+
+#Preview {
+    SearchView()
+}
