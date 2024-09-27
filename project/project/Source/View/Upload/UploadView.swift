00dbde3dae81b8a4df929bfc82f4c276d1a55720
CJiu01 <jiwoo020801@gmail.com>
2024-09-27 17:33:20 +0900

    feat: UploadView 추가

--- project/project/Source/View/Upload/UploadView.swift
+++ project/project/Source/View/Upload/UploadView.swift
@@ -5,4 +5,15 @@
 //  Created by 최지우 on 9/27/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct UploadView: View {
+    var body: some View {
+        Text("UploadView")
+    }
+}
+
+#Preview {
+    UploadView()
+}
+
