f154faad4273c75bdb8e57c0d5859958254d514a
JaeSunEo <wotjs020708@gmail.com>
2024-09-28 01:19:16 +0900

    Test: Animation Test

--- project/project/Source/View/Following/FollowingDetailView.swift
+++ project/project/Source/View/Following/FollowingDetailView.swift
@@ -9,11 +9,18 @@ import SwiftUI
 
 struct FollowingDetailView: View {
     var item: Int
+    @State private var showModel = true
     var body: some View {
         ZStack {
             Color.black.opacity(0.1)
                 .ignoresSafeArea(.all)
+            
+            if showModel {
+                Color.black.opacity(0.5)
+                    .ignoresSafeArea()
+            }
             VStack{
+                
                 Spacer()
                 HStack {
                     VStack(alignment: .leading) {
@@ -50,7 +57,69 @@ struct FollowingDetailView: View {
             
             }
         }
+        .fullScreenCover(isPresented: $showModel) {
+            ModalView(showModal: $showModel)
+        }
         .toolbar(.hidden,for: .tabBar)
+    }
+}
+
+struct ModalView: View {
+    @Binding var showModal: Bool
+    
+    var body: some View {
+        ZStack {
+            LottieView(jsonName:"cloud")
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                
+                Image(systemName: "person.crop.circle")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Text("종강하는길 🥱")
+                    .font(.title)
+                    .padding()
+                
+                Text("2024.06.30 작성 • 2024.09.28 오픈")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                
+                Text("이걸 열어볼 때 쯤이면 이미 개강하고 정신없이 바쁜 하루를 보내고 있겠지.. 종강만 기다려요...")
+                    .padding()
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                
+              
+            }
+        }
+        .background(ClearBackground())
+    }
+}
+
+struct ClearBackground: UIViewRepresentable {
+    
+    public func makeUIView(context: Context) -> UIView {
+        
+        let view = ClearBackgroundView()
+        DispatchQueue.main.async {
+            view.superview?.superview?.backgroundColor = .clear
+        }
+        return view
+    }
+
+    public func updateUIView(_ uiView: UIView, context: Context) {}
+}
+
+class ClearBackgroundView: UIView {
+    open override func layoutSubviews() {
+        guard let parentView = superview?.superview else {
+            return
+        }
+        parentView.backgroundColor = .clear
     }
 }
 
