a96c74324f1a09dc298418c6f6266f75ada01c45
CJiu01 <jiwoo020801@gmail.com>
2024-09-27 17:32:35 +0900

    feat: MainTabType case 추가

--- project/project/Source/View/Tab/MainTabType.swift
+++ project/project/Source/View/Tab/MainTabType.swift
@@ -9,17 +9,23 @@ import Foundation
 
 enum MainTabType: String, CaseIterable {    // CaseIterable : 뷰에서 ForEach로 뷰 그리기
     case home
+    case following
+    case search
+    case upload
     case mypage
     
     var title: String {
         switch self {
         case .home:
-            return "홈"
+            return "home"
+        case .following:
+            return "following"
+        case .search:
+            return "search"
+        case .upload:
+            return "upload"
         case .mypage:
-            return "내 프로필"
+            return "mypage"
         }
     }
 }
-
-
-
