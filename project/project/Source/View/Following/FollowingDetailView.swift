64eb517aedb15b52fe3bbcf2f8a0378cf9cf6881
JaeSunEo <wotjs020708@gmail.com>
2024-09-28 03:05:16 +0900

    design: UI 변경

--- project/project/Source/View/Following/FollowingDetailView.swift
+++ project/project/Source/View/Following/FollowingDetailView.swift
@@ -8,35 +8,50 @@
 import SwiftUI
 
 struct FollowingDetailView: View {
+    @Environment(\.dismiss) var dismiss
     var item: Int
     @State private var showModel = true
     var body: some View {
         ZStack {
-            Color.black.opacity(0.1)
-                .ignoresSafeArea(.all)
+            Image("capsule")
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+                .frame(width: 300)
             
             VStack{
-                
+                HStack{
+                    Button {
+                        self.dismiss()
+                    } label: {
+                        Image(systemName: "chevron.left")
+                            .foregroundStyle(Color.background)
+                            .padding(20)
+                    }
+                    Spacer()
+                }
                 Spacer()
                 HStack {
                     VStack(alignment: .leading) {
                         Text("타이틀")
-                            .font(.system(size: 22))
+                            .font(.title)
+                            .foregroundStyle(.background)
                         ZStack {
-                            Rectangle()
-                                .frame(width: 160, height: 40)
-                                .foregroundStyle(Color.primary.opacity(0.2))
-                                .cornerRadius(20)
+                            
                             HStack{
                                 Image(systemName: "person.crop.circle")
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 24)
                                 Text("2024.09.25 캠출 오픈")
-                                    .font(.system(size: 12))
+                                    .font(.body4)
                                     .foregroundStyle(.white)
-                                
                             }
+                            .padding(EdgeInsets(top: 3, leading: 4, bottom: 3, trailing: 11))
+                            .background(Color.primary.opacity(0.8))
+                            .cornerRadius(30)
+                            
+                            
                         }
                     }
                     Spacer()
@@ -49,10 +64,12 @@ struct FollowingDetailView: View {
                     }
                     .foregroundStyle(.white)
                 }
-                .padding()
-            
+                .padding(20)
+                
             }
         }
+        .navigationBarBackButtonHidden(true)
+//        .background(Color.basicBlack.ignoresSafeArea())
         .fullScreenCover(isPresented: $showModel) {
             ModalView(showModal: $showModel)
         }
