eede8240d65eca7722ce204901291b0c01c8c52e
JaeSunEo <wotjs020708@gmail.com>
2024-09-29 01:45:11 +0900

    feat: add FriendView

---
+++ project/project/Source/View/Friend/FriendView.swift
@@ -0,0 +1,18 @@
+//
+//  FriendView.swift
+//  project
+//
+//  Created by 어재선 on 9/29/24.
+//
+
+import SwiftUI
+
+struct FriendView: View {
+    var body: some View {
+       
+    }
+}
+
+#Preview {
+    FriendView()
+}
