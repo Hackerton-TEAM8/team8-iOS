8ce929c134a7516381e111d1ba301b943c05770c
JaeSunEo <wotjs020708@gmail.com>
2024-09-29 06:38:38 +0900

    feat: myPage UI 작업

--- project/project/Source/View/MyPage/MyPageView.swift
+++ project/project/Source/View/MyPage/MyPageView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct MyPageView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Image("myPage 1")
+                .resizable()
+                .scaledToFill()
+        }
     }
 }
 
