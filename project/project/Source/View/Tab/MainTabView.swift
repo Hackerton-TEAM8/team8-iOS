e1e2916f6159190bb88d80c4ed7e4b63c9e74a82
CJiu01 <jiwoo020801@gmail.com>
2024-09-28 21:02:25 +0900

    feat: tabView 디자인 적용

--- project/project/Source/View/Tab/MainTabView.swift
+++ project/project/Source/View/Tab/MainTabView.swift
@@ -20,7 +20,7 @@ struct MainTabView: View {
                     switch tab {
                     case .home:
                         HomeView()
-                    case .following:
+                    case .feed:
                         FollowingView()
                     case .search:
                         SearchView()
@@ -34,11 +34,44 @@ struct MainTabView: View {
                 .tabItem {
                     Label(
                         title: { Text(tab.title) },
-                        icon: { Image(systemName: "42.circle") }
+                        icon: { Image(getTabImage(tab: tab)) }
                     )
                     
                 }
                 .tag(tab)
+            }
+        }
+        .accentColor(.black)
+    }
+    
+    // 선택된 탭에 따라 다른 이미지 반환
+    func getTabImage(tab: MainTabType) -> String {
+        if tab == selectedTab {
+            switch tab {
+            case .home:
+                return "home_sel"
+            case .feed:
+                return "feed_sel"
+            case .search:
+                return "search_sel"
+            case .upload:
+                return "upload_sel"
+            case .mypage:
+                return "mypage_sel"
+            }
+        } else {
+            // 비활성화된 상태의 이미지
+            switch tab {
+            case .home:
+                return "home"
+            case .feed:
+                return "feed"
+            case .search:
+                return "search"
+            case .upload:
+                return "upload"
+            case .mypage:
+                return "mypage"
             }
         }
     }
